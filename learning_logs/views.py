b6347aa8e888e36bfa45ff7a6a571436115670ae
allysonlipe <allysongunner1@hotmail.com>
2024-07-27 15:13:59 -0300

    primeira view criada

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    """Página Principal do Learning Log"""
+    return render(request, 'learning_logs/index.html')
