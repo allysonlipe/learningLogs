ce1218253c51643810c6f378d34c5870b38cda69
allysonlipe <allysongunner1@hotmail.com>
2024-08-20 15:42:12 -0300

    reinicio do projeto

--- learning_logs/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    """Página Principal do Learning Log"""
-    return render(request, 'learning_logs/index.html')
